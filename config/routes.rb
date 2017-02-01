3c6dc3b9c4a00008a98cf7c978266cdd7b359559
memaker <roberto.bartolome.medina@gmail.com>
2017-02-01 21:55:22 +0100

    Simple Publications up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  resources :publications
+
+  root to: 'publications#index'
   devise_for :users
   resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224203311) do
+ActiveRecord::Schema.define(version: 20170128210057) do
+
+  create_table "publications", force: :cascade do |t|
+    t.string   "title"
+    t.string   "authors"
+    t.string   "pubmed_id"
+    t.decimal  "impact_factor"
+    t.date     "date_of_print"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
